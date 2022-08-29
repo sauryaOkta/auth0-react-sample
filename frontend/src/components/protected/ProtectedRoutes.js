1902afd6b445ab35df98962c0c0cbe2b3b23200d
sauryaOkta <saurya.pandey@okta.com>
2022-08-29 20:46:22 +0545

    generic0

--- frontend/src/components/protected/ProtectedRoutes.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
-
-const ProtectedRoutes = ({ component, ...props }) => {
-  const ProtectedComponent = withAuthenticationRequired(component);
-  return <ProtectedComponent {...props} />;
-};
-
-export default ProtectedRoutes;
--- frontend/src/util/config.js
+++
@@ -1,9 +0,0 @@
-export function getConfig() {
-  const audience = process.env.REACT_APP_OKTA_AUDIENCE;
-
-  return {
-    domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
-    ...(audience ? { audience } : null),
-  };
-}
